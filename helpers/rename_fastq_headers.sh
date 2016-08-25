0ef37431fcf2e016acb7f3334e8cff33cfa488a0
Milt Epstein <mepstein@illinois.edu>
2016-08-25 14:51:38 +0000

    Added use of  in rename_fastq_headers.sh

--- helpers/rename_fastq_headers.sh
+++ helpers/rename_fastq_headers.sh
@@ -6,24 +6,29 @@
 # $4 = fw_read_renamed
 # $5 = rv_read_renamed
 
+if [ -z "$TMPDIR" ]; then
+    TMPDIR=/tmp
+fi
+
 echo $1
 echo $2
 echo $3
 echo $4
 echo $5
+echo $TMPDIR
 
 # fastx_toolkit needs to be setup in PATH
 
 # Sort out forward read
 basename=`basename $2`
-fastx_renamer -Q33 -n COUNT -i $2 -o /tmp/${basename}_tmp
-sed "s/^\(@\|+\)\([0-9]*\)$/\1$1\2;barcodelabel=$1\/1/" /tmp/${basename}_tmp > /tmp/${basename}_renamed
-rm -f /tmp/${basename}_tmp
-mv /tmp/${basename}_renamed $4
+fastx_renamer -Q33 -n COUNT -i $2 -o $TMPDIR/${basename}_tmp
+sed "s/^\(@\|+\)\([0-9]*\)$/\1$1\2;barcodelabel=$1\/1/" $TMPDIR/${basename}_tmp > $TMPDIR/${basename}_renamed
+rm -f $TMPDIR/${basename}_tmp
+mv $TMPDIR/${basename}_renamed $4
 
 # Sort out reverse read
 basename=`basename $3`
-fastx_renamer -Q33 -n COUNT -i $3 -o /tmp/${basename}_tmp
-sed "s/^\(@\|+\)\([0-9]*\)$/\1$1\2;barcodelabel=$1\/1/" /tmp/${basename}_tmp > /tmp/${basename}_renamed
-rm -f /tmp/${basename}_tmp
-mv /tmp/${basename}_renamed $5
+fastx_renamer -Q33 -n COUNT -i $3 -o $TMPDIR/${basename}_tmp
+sed "s/^\(@\|+\)\([0-9]*\)$/\1$1\2;barcodelabel=$1\/1/" $TMPDIR/${basename}_tmp > $TMPDIR/${basename}_renamed
+rm -f $TMPDIR/${basename}_tmp
+mv $TMPDIR/${basename}_renamed $5
