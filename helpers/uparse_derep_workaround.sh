9e69eea6632b31a3b563367a154fdbe81f03bad2
ShakunBaichoo <shakunb@uom.ac.mu>
2016-08-25 12:08:11 +0400

    Updated script

--- helpers/uparse_derep_workaround.sh
+++ helpers/uparse_derep_workaround.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# $1 = dir with individual fasta files
-# $2 = dereplicated fasta file
+# $* = individual array of fasta files
 
-echo $1
-echo $2
-
-cat $1/*.fasta | grep -v "^>" | grep -v [^ACGTacgt] | sort -d | uniq -c | while read abundance sequence ; do hash=$(printf "${sequence}" | sha1sum); hash=${hash:0:40};printf ">%s;size=%d;\n%s\n" "${hash}" "${abundance}" "${sequence}"; done > $3 2> /dev/null 
-
-
-
+cat $* | grep -v "^>" | grep -v [^ACGTacgt] | sort -d | uniq -c | \
+while read abundance sequence ; do hash=$(printf "${sequence}" | sha1sum);\
+ hash=${hash:0:40};printf ">%s;size=%d;\n%s\n" "${hash}" "${abundance}" "${sequence}"; done > derep.fasta
