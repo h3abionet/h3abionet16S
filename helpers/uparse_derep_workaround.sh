422a239c27d53d3bd072548957302ef6f6bdef87
Gerrit Botha <gerrit.botha@gmail.com>
2016-08-25 09:42:51 +0200

    Now we only have a input directory with *.fasta files as input and an output fasta file.

--- helpers/uparse_derep_workaround.sh
+++ helpers/uparse_derep_workaround.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
 # $1 = dir with individual fasta files
-# $2 = file file individual files concatenated
-# $3 = dereplicated fasta file
-# $4 = error file 
+# $2 = dereplicated fasta file
 
 echo $1
 echo $2
-echo $3
-echo $4
 
-cat $1/*.fasta > $2
-
-cat $1 | grep -v "^>" | grep -v [^ACGTacgt] | sort -d | uniq -c | while read abundance sequence ; do hash=$(printf "${sequence}" | sha1sum); hash=${hash:0:40};printf ">%s;size=%d;\n%s\n" "${hash}" "${abundance}" "${sequence}"; done > $3 2> $4
+cat $1/*.fasta | grep -v "^>" | grep -v [^ACGTacgt] | sort -d | uniq -c | while read abundance sequence ; do hash=$(printf "${sequence}" | sha1sum); hash=${hash:0:40};printf ">%s;size=%d;\n%s\n" "${hash}" "${abundance}" "${sequence}"; done > $3 2> /dev/null 
 
 
 
