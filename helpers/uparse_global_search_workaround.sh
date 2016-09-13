70237770b0a8b642b95375c592df2d4aa6d355ef
Gerrit Botha <gerrit.botha@gmail.com>
2016-09-13 15:20:58 +0000

    Changed the output naming.

--- helpers/uparse_global_search_workaround.sh
+++ helpers/uparse_global_search_workaround.sh
@@ -15,7 +15,7 @@ fasta-splitter.pl -n-parts-total 100 -out-dir split $1
 cd split
 
 for i in $(ls *.fasta);
-  do usearch8 -usearch_global $i -db $2 -id $3 -strand $4 -uc $i.map.uc;
+  do usearch8 -usearch_global $i -db $2 -id $3 -strand $4 -uc $i.uc;
 done 
 
-cat *.map.uc > ../otus.mapped.uc
+cat *.uc > ../otus.uc
