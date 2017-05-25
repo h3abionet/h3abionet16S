1a7f1e59b7241c5d051a4fb9ef52eb5ccf1a1e1c
Gerrit Botha <gerrit.botha@uct.ac.za>
2017-05-25 17:46:54 +0200

    Added part that does multi-line fasta to single-line fasta conversion.

--- helpers/uparse_derep_workaround.sh
+++ helpers/uparse_derep_workaround.sh
@@ -2,6 +2,7 @@
 
 # $* = individual array of fasta files
 
-cat $* | grep -v "^>" | grep -v [^ACGTacgt] | sort -d | uniq -c | \
+cat $* | awk '/^>/ {printf("%s%s\n",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' | \
+grep -v "^>" | grep -v [^ACGTacgt] | sort -d | uniq -c | \
 while read abundance sequence ; do hash=$(printf "${sequence}" | sha1sum);\
  hash=${hash:0:40};printf ">%s;size=%d;\n%s\n" "${hash}" "${abundance}" "${sequence}"; done > derep.fasta
