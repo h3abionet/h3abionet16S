4af94ec9e7e7b67337a40d7847ad847c08b2c677
Gerrit Botha <gerrit.botha@gmail.com>
2017-10-10 16:04:35 +0200

    Added hack so that Nextflow can access the db.
    Hopefully this will not break CWL runs. Need to implement this better later.

--- helpers/uparse_global_search_workaround.sh
+++ helpers/uparse_global_search_workaround.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# $1 = fasta input file 
+# $1 = fasta input file
 # $2 = representative OTU fasta set
 # $3 = percentage identity (float e.g. 0.97)
 # $4 = strand (string e.g. plus)
@@ -14,8 +14,11 @@ fasta-splitter.pl -n-parts-total 100 -out-dir split $1
 
 cd split
 
+# The nextline is just a hack for now to get Nextflow to be able to access the $db
+db=`readlink -f $2`
+
 for i in $(ls *.fasta);
-  do usearch -usearch_global $i -db $2 -id $3 -strand $4 -uc $i.uc;
-done 
+  do usearch -usearch_global $i -db $db -id $3 -strand $4 -uc $i.uc;
+done
 
 cat *.uc > ../otus.uc
