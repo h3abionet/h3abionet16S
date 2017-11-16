4be4bdaa1ca9b8f42aa8840916feb06214b593b5
Gerrit Botha <gerrit.botha@gmail.com>
2017-11-16 14:51:08 +0200

    Removing the automation code.
    Not longer maintained and not sure if it is still working.

--- automation/rdna/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-############################################################
-# Run workflows
-############################################################
-
-cd ~/h3abionet16S/workflows-docker
-ln -sf /home/ubuntu/input/dog_stool_samples/ .
-/home/ubuntu/.local/bin/cwltool --outdir ~/output demo-docker.cwl demo-docker.input.yml
--- automation/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-############################################################
-# Run workflows
-############################################################
-
-cd ~/h3abionet16S/workflows-docker
-ln -s /home/ubuntu/input/dog_stool_samples/ .
-cwltool --outdir ~/output demo-docker.cwl demo-docker.input.yml
